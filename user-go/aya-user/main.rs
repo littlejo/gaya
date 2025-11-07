fb07fc816d897d10d3530280e8f12f7ccbdeb595
Joseph Ligier <joseph.ligier@accenture.com>
2025-11-07 18:55:28 +0100

    Add main.rs

---
+++ user-go/aya-user/main.rs
@@ -0,0 +1,22 @@
+#[rustfmt::skip]
+use log::debug;
+use std::fs;
+
+fn main() -> anyhow::Result<()> {
+    // Bump the memlock rlimit. This is needed for older kernels that don't use the
+    // new memcg based accounting, see https://lwn.net/Articles/837122/
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {ret}");
+    }
+
+    let src = concat!(env!("OUT_DIR"), "/test-go");
+    let dst = "/tmp/test-go";
+
+    fs::copy(src, dst)?;
+    Ok(())
+}
